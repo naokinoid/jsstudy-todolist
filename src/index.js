dddefa90d2af14357cced77f00b0c49987277fde
Naoki Ito <66786623+naokinoid@users.noreply.github.com>
2022-03-12 23:29:02 +0900

    add-button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの入力値を取得し、初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // divタグを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグを生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの小要素にliタグを追加
+  div.appendChild(li);
+
+  // id = incomplete-listの要素（ulタグ）の小要素にdivタグを追加
+  document.getElementById("incomplete-list").appendChild(div);
+
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
